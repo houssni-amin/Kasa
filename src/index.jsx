22b24418d9f3709495d14cdf5db3717450056775
houssni-amin <houssni.amin@outlook.fr>
2024-02-01 16:15:55 +0100

    Mise en place de React Router avec toutes les pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import Home from './pages/Home'
+import Propos from './pages/Propos'
+import Logement from './pages/Logement'
+import Footer from './components/Footer'
+import Error from './components/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/propos" element={<Propos />} />
+        <Route path="/logement" element={<Logement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>,
 )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 function Home() {
-  return <div>Hello World</div>
+  return <h1>Home</h1>
 }
 
 export default Home

d5c22cf8ba1939a62050c63c404c9c3179b15978
houssni-amin <houssni.amin@outlook.fr>
2024-02-01 21:19:43 +0100

    Création page error avec le CSS

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+import './error.css'
+
 function Error() {
-  return <h1>404</h1>
+  return (
+    <div className="error-container">
+      <h1 className="error-h1">404</h1>
+      <p className="error-text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to={'/'} className="error-button">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  )
 }
 
 export default Error

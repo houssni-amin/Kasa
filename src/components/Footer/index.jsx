2357f29d5d0d1d4be18c03ffa85073950d78b7cf
houssni-amin <houssni.amin@outlook.fr>
2024-02-01 21:16:37 +0100

    Création du Footer avec le CSS

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,13 @@
+import './footer.css'
+import FooterLogo from '../../assets/FooterLogo.svg'
+
 function Footer() {
-  return <h1>Footer</h1>
+  return (
+    <div className="footer-container">
+      <img src={FooterLogo} alt="Logo Kasa" className="footer-logo" />
+      <p className="footer-text">© 2020 Kasa. All rights reserved</p>
+    </div>
+  )
 }
 
 export default Footer

88fdab1c453c52dfc0def95efdc35ee85cf87fa3
houssni-amin <houssni.amin@outlook.fr>
2024-02-01 21:13:41 +0100

    CSS Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom'
+import './header.css'
+import HeaderLogo from '../../assets/HeaderLogo.svg'
 
 function Header() {
   return (
-    <nav>
-      <Link to={'/'}>Accueil</Link>
-      <Link to={'/propos'}>A Propos</Link>
-    </nav>
+    <div className="header-container">
+      <img src={HeaderLogo} alt="Logo Kasa" className="header-logo" />
+      <nav className="header-nav">
+        <Link to={'/'} className="header-button button-left">
+          Accueil
+        </Link>
+        <Link to={'/propos'} className="header-button button-right">
+          A Propos
+        </Link>
+      </nav>
+    </div>
   )
 }
 

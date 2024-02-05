68e398e379cda8a6d67ed2094c72d191f90a77b6
houssni-amin <houssni.amin@outlook.fr>
2024-02-05 11:08:59 +0100

    Ajout du mode navigation actif pour le soulignement du header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './header.css'
 import HeaderLogo from '../../assets/HeaderLogo.svg'
 
@@ -7,12 +7,12 @@ function Header() {
     <div className="header-container">
       <img src={HeaderLogo} alt="Logo Kasa" className="header-logo" />
       <nav className="header-nav">
-        <Link to={'/'} className="header-button button-left">
+        <NavLink to={'/'} className="header-button button-left">
           Accueil
-        </Link>
-        <Link to={'/propos'} className="header-button button-right">
+        </NavLink>
+        <NavLink to={'/propos'} className="header-button button-right">
           A Propos
-        </Link>
+        </NavLink>
       </nav>
     </div>
   )

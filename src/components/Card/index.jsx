837ddd9a607816c6ed00c780d69ea94b887469af
houssni-amin <houssni.amin@outlook.fr>
2024-02-05 11:18:07 +0100

    Création Card avec le css

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+function Card({ title, cover }) {
+  return (
+    <div className="card-container">
+      <img src={cover} alt="Cover" className="card-cover" />
+      <div className="deg"></div>
+      <p className="card-title">{title}</p>
+    </div>
+  )
+}
+
+export default Card

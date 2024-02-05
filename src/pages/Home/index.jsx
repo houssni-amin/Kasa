57c097f994368596f0ffa57afbbc659a320bd7d9
houssni-amin <houssni.amin@outlook.fr>
2024-02-05 11:10:24 +0100

    Création page Home avec le css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,21 @@
+import Card from '../../components/Card'
+import logements from '../../data/logements.json'
+import './home.css'
+
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className="home-container">
+      <div className="home-card">
+        {logements.map((logement) => (
+          <Card
+            key={logement.id}
+            cover={logement.cover}
+            title={logement.title}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Home

1b5b199319bae88842d4103ba0541d8eef52520a
houssni-amin <houssni.amin@outlook.fr>
2024-02-07 15:23:43 +0100

    Optimisation du Header et Ajout de la compatibilité responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import HeaderLogo from '../../assets/HeaderLogo.svg'
 function Header() {
   return (
     <div className="header-container">
-      <img src={HeaderLogo} alt="Logo Kasa" className="header-logo" />
+      <img src={HeaderLogo} alt="Logo Kasa" />
       <nav className="header-nav">
         <NavLink to={'/'} className="header-button button-left">
           Accueil

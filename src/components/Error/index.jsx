0c9d473badc42c847f7ef19e387bc301f8a74e67
houssni-amin <houssni.amin@outlook.fr>
2024-02-08 15:18:58 +0100

    Optimisation page Error et Ajout de la compatibilité responsive

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -4,10 +4,8 @@ import './error.css'
 function Error() {
   return (
     <div className="error-container">
-      <h1 className="error-h1">404</h1>
-      <p className="error-text">
-        Oups! La page que vous demandez n'existe pas.
-      </p>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
       <Link to={'/'} className="error-button">
         Retourner sur la page d'accueil
       </Link>
